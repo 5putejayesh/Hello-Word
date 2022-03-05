a6f1db7806c2784e48925ace203d1a2ee7b1f6ed
Jayesh 5pute <5putejayesh@gmail.com>
2022-03-05 22:28:08 +0530

    Order and OrderDao modified

--- src/dao/OrderDao.java
+++ src/dao/OrderDao.java
@@ -2,13 +2,24 @@ package src.dao;
 
 import src.model.Order;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderDao {
 
+    private CustomerDAO customerDAO=new CustomerDAO();
+    private ProductDao productDao=new ProductDao();
+
     public List<Order> getOrders(){
         List<Order> orderList=new ArrayList<>();
+        orderList.add(new Order(1,"Deliverd",
+                LocalDate.of(2022,02,01),
+                LocalDate.of(2022,02,05),
+                productDao.getAllProducts().stream().filter(p->p.getCategory().equalsIgnoreCase("Books"))
+                        .collect(Collectors.toList()),
+                customerDAO.getAllCustomers().get(0)));
         return orderList;
     }
 }
--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,6 @@
 package src.model;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -7,13 +8,13 @@ import java.util.Objects;
 public class Order {
     private long id;
     private String status;
-    private Date orderDate;
-    private Date deliveryDate;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
     private List<Product> products;
     private Customer customer;
 
 
-    public Order(long id, String status, Date orderDate, Date deliveryDate, List<Product> products, Customer customer) {
+    public Order(long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
         this.id = id;
         this.status = status;
         this.orderDate = orderDate;
