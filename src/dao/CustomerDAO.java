6caf706e210d470129d99bf1798ba2101759062a
Jayesh 5pute <5putejayesh@gmail.com>
2022-03-05 12:33:24 +0530

    getALLcust added

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -1,4 +1,14 @@
 package src.dao;
 
+import src.model.Customer;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CustomerDAO {
+
+    public List<Customer> getAllCustomers(){
+        return Arrays.asList(new Customer(1,"Jayesh",1),
+                new Customer(2,"Nilesh",2));
+    }
 }
