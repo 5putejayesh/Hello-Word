a89612b4c9430b7912e2c2b119d88607b6a68003
Jayesh 5pute <5putejayesh@gmail.com>
2022-03-05 22:04:36 +0530

    Order Dao added

---
+++ src/dao/OrderDao.java
@@ -0,0 +1,14 @@
+package src.dao;
+
+import src.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDao {
+
+    public List<Order> getOrders(){
+        List<Order> orderList=new ArrayList<>();
+        return orderList;
+    }
+}
